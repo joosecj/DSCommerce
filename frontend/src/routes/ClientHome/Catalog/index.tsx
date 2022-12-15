132122ac2407c3cd65f94d034122d2207ddfb08b
Jose Carlos <josecarloscjj@gmail.com>
2022-12-15 17:07:08 -0300

    useEffect to products and details

--- frontend/src/routes/ClientHome/Catalog/index.tsx
+++ frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -2,17 +2,28 @@ import './styles.css';
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNexPage from '../../../components/ButtonNextPage';
-import * as productService from '../../../services/product-service';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/products';
+import axios from 'axios';
 
 
 export default function Catalog() {
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/products?size=12")
+      .then(response => {
+        setProducts(response.data.content);
+      })
+  }, [])
+
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {
-            productService.findAll().map(
+            products.map(
               product => <CatalogCard key={product.id} product={product} />
             )
           }
--- frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,13 +2,23 @@ import './styles.css';
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from '../../../services/product-service';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/products';
+import axios from 'axios';
 
 export default function ProductDetails() {
   const params = useParams();
-  const product = productService.findById(Number(params.productId));
+
+  const [product, setProduct] = useState<ProductDTO>();
+  useEffect(() => {
+    axios.get(`http://localhost:8080/products/${params.productId}`)
+      .then(response => {
+        setProduct(response.data);
+      })
+
+  }, [])
 
   return (
     <main>
