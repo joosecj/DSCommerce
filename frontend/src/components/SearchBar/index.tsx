fdd8d1f7a3a3c947b7d98d2edb2d8718fc1a6b94
Jose Carlos <josecarloscjj@gmail.com>
2023-01-29 14:37:11 -0300

    Searchbar Onsearch event

--- frontend/src/components/SearchBar/index.tsx
+++ frontend/src/components/SearchBar/index.tsx
@@ -1,11 +1,39 @@
+import { useState } from 'react';
 import './styles.css';
 
-export default function SearchBar() {
+type Props = {
+  onSearch: Function
+}
+
+export default function SearchBar({ onSearch }: Props) {
+
+  const [text, setText] = useState("");
+
+  function handleChange(event: any) {
+    setText(event.target.value);
+  }
+
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    onSearch(text)
+  }
+
+  function handleResetClick() {
+    setText("");
+    onSearch(text);
+  }
+
   return (
-    <form className="dsc-search-bar">
+    <form className="dsc-search-bar" onSubmit={handleSubmit}>
       <button type="submit">🔎︎</button>
-      <input type="text" placeholder="Nome do produto" />
-      <button type="reset">🗙</button>
+      <input
+        value={text}
+        type="text"
+        placeholder="Nome do produto"
+        onChange={handleChange}
+      />
+      <button onClick={handleResetClick}>❌</button>
     </form>
   );
 }
--- frontend/src/routes/ClientHome/Catalog/index.tsx
+++ frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -9,19 +9,25 @@ import * as productService from '../../../services/product-service';
 
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
+  const [productName, setProductsName] = useState("");
+
 
   useEffect(() => {
-    productService.findAll()
+    productService.findPageRequest(0, productName)
       .then(response => {
         setProducts(response.data.content);
       })
-  }, [])
+  }, [productName]);
+
+  function handleSearch(searchText: string) {
+    setProductsName(searchText)
+  }
 
 
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {
             products.map(
