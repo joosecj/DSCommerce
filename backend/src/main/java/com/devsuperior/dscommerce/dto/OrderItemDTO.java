95281232a3d8f51f59885b61909f823efce3aeb1
Jose Carlos <josecarloscjj@gmail.com>
2023-02-11 15:20:36 -0300

    bug: return imgUrl

--- backend/src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ backend/src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -7,12 +7,14 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDTO(OrderItem orderItemEntity) {
@@ -20,6 +22,7 @@ public class OrderItemDTO {
         name = orderItemEntity.getProduct().getName();
         price = orderItemEntity.getPrice();
         quantity = orderItemEntity.getQuantity();
+        imgUrl = orderItemEntity.getProduct().getImgUrl();
     }
 
     public Long getProductId() {
@@ -38,7 +41,12 @@ public class OrderItemDTO {
         return quantity;
     }
 
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
     public Double getSubtotal() {
         return price * quantity;
     }
+
 }
