a203499748ddfccc6acc9616070fd8c07c064857
Jose Carlos <josecarloscjj@gmail.com>
2023-02-16 16:38:45 -0300

    Forms update

--- frontend/src/routes/ClientHome/Login/index.tsx
+++ frontend/src/routes/ClientHome/Login/index.tsx
@@ -1,6 +1,6 @@
 import './styles.css';
 import { useContext, useState } from 'react';
-import { CredentialsDTO } from '../../../models/auth';
+import * as forms from '../../../utils/forms';
 import * as authService from '../../../services/auth-service'
 import { useNavigate } from 'react-router-dom';
 import { ContextToken } from '../../../utils/context-token';
@@ -45,7 +45,7 @@ export default function Login() {
   function handleInputChange(event: any) {
     const value = event.target.value;
     const name = event.target.name;
-    setFormData({ ...formData, [name]: { ...formData[name], value: value } });
+    setFormData(forms.update(formData, name, value));
   }
   return (
     <main>
